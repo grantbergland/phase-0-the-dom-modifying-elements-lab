9cfc3ec9e58de539e58916519a4d3e388565e913
grantbergland <grantbergland@gmail.com>
2022-08-16 05:55:35 -0500

    commit tree cdba1fba0ce3c46ce66ca3594dc649fac815af03

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.innerHTML =
+  "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
